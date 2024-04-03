ed115b6fbfceb1e96666b50503cbdddf498fd504
rakin374 <rakin374@gmail.com>
2024-04-02 22:39:02 -0400

    Initial Commit

---
+++ xivNous/xivNousApp.swift
@@ -0,0 +1,17 @@
+//
+//  xivNousApp.swift
+//  xivNous
+//
+//  Created by Rakin Munim on 4/2/24.
+//
+
+import SwiftUI
+
+@main
+struct xivNousApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
